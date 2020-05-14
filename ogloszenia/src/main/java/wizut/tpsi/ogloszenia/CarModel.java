2213f35818bbe535ed22dd871ab024d409a7abf1
Jakubrutkowski <jakubrutkowski95@gmail.com>
2020-05-14 14:36:54 +0200

    Update CarModel.java

--- ogloszenia/src/main/java/wizut/tpsi/ogloszenia/CarModel.java
+++ ogloszenia/src/main/java/wizut/tpsi/ogloszenia/CarModel.java
@@ -5,10 +5,41 @@
  */
 package wizut.tpsi.ogloszenia;
 
-/**
- *
- * @author komp
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "car_model")
 public class CarModel {
+    
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Integer id;
+    
+    @Size(max=30)
+    @Column(name = "name")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
 }
