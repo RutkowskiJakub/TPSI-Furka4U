bc7fdc2ae4b4014bd503b358a84b960ece126ec4
Jakubrutkowski <jakubrutkowski95@gmail.com>
2020-05-14 15:17:46 +0200

    Dodanie metody getModel

--- ogloszenia/src/main/java/wizut/tpsi/ogloszenia/services/OffersService.java
+++ ogloszenia/src/main/java/wizut/tpsi/ogloszenia/services/OffersService.java
@@ -9,6 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Service;
 import wizut.tpsi.ogloszenia.jpa.CarManufacturer;
+import wizut.tpsi.ogloszenia.jpa.CarModel;
 
 
 
@@ -23,4 +24,7 @@ public class OffersService {
         return em.find(CarManufacturer.class, id);
     }
     
+    public CarModel getModel(int id){
+        return em.find(CarModel.class, id);
+    }
 }
