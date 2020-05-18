2442efd7855ab0ddb6b934130f24a4b8d554fdc7
Jakubrutkowski <jakubrutkowski95@gmail.com>
2020-05-18 23:42:41 +0200

    Dodanie metod pobierajacych fueltypes, carmodels i bodystyles

--- ogloszenia/src/main/java/wizut/tpsi/ogloszenia/services/OffersService.java
+++ ogloszenia/src/main/java/wizut/tpsi/ogloszenia/services/OffersService.java
@@ -10,8 +10,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import org.springframework.stereotype.Service;
+import wizut.tpsi.ogloszenia.jpa.BodyStyle;
 import wizut.tpsi.ogloszenia.jpa.CarManufacturer;
 import wizut.tpsi.ogloszenia.jpa.CarModel;
+import wizut.tpsi.ogloszenia.jpa.FuelType;
 
 
 
@@ -34,6 +36,27 @@ public class OffersService {
         String jpql = "select cm from CarManufacturer cm order by cm.name";
         TypedQuery<CarManufacturer> query = em.createQuery(jpql, CarManufacturer.class);
         List<CarManufacturer> result = query.getResultList();
+        return result;
+    }
+    
+    public List<BodyStyle> getBodyStyles(){
+        String jpql = "select cm from BodyStyle cm order by cm.name";
+        TypedQuery<BodyStyle> query = em.createQuery(jpql, BodyStyle.class);
+        List<BodyStyle> result = query.getResultList();
+        return result;
+    }
+    
+    public List<FuelType> getFuelTypes(){
+        String jpql = "select cm from FuelType cm order by cm.name";
+        TypedQuery<FuelType> query = em.createQuery(jpql, FuelType.class);
+        List<FuelType> result = query.getResultList();
+        return result;
+    }
+    
+    public List<CarModel> getCarModels(){
+        String jpql = "select cm from CarModel cm order by cm.name";
+        TypedQuery<CarModel> query = em.createQuery(jpql, CarModel.class);
+        List<CarModel> result = query.getResultList();
         return result;
     }
 }
